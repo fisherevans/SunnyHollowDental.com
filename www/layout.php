2796aca0ddc396b3eb3dfe156b21667f2311dc09
Fisher Evans <contact@fisherevans.com>
2015-09-17 11:15:00 -0400

    home page regions polish

--- www/layout.php
+++ www/layout.php
@@ -1,43 +1,40 @@
-<?php
-    global $url;
-    $title = (isset($title) ? $title . ' | ' : '') . 'Sunny Hollow Dental';
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <!-- Meta-Data -->
-    <title><?=$title?></title>
+    <title><?=(isset($title) ? $title . ' | ' : '') . 'Sunny Hollow Dental'?></title>
     <!-- Styling -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:100,300,400,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" type="text/css" href="lib/flaticon/flaticon.css">
-    <link href="style.php/combined.scss" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:100,300,400,700">
+    <link rel="stylesheet" type="text/css" href="/lib/flaticon/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="/style.php/combined.scss" />
 </head>
 <body>
 <div class="wrapper">
     <div class="header">
-        <img class="browser-banner" src="img/banner.png" alt="Sunny Hollow Dental Banner" />
+        <img class="browser-banner" src="/img/banner.png" alt="Sunny Hollow Dental Banner" />
         <div class="mobile-banner">
-            <img class="mobile-logo" src="img/mobile-logo.png" alt="SHD Logo" />
-            <img class="mobile-text" src="img/mobile-text.png" alt="Sunny Hollow Dental" />
-            <div class="mobile-nav-toggle fadeColors noUserSelect">
-                <div class="show-nav flaticon-menu48 fadeColors"></div>
-                <div class="hide-nav flaticon-delete30 fadeColors"></div>
+            <img class="mobile-logo" src="/img/mobile-logo.png" alt="SHD Logo" />
+            <img class="mobile-text" src="/img/mobile-text.png" alt="Sunny Hollow Dental" />
+            <div class="mobile-nav-toggle fade-colors no-user-select">
+                <div class="show-nav flaticon-menu48 fade-colors"></div>
+                <div class="hide-nav flaticon-delete30 fade-colors"></div>
             </div>
         </div>
     </div>
     <div class="navigation">
-        <div class="element first"><a href="<?=$url?>/">Home</a></div>
-        <div class="element"><a href="<?=$url?>/news">News</a></div>
+        <div class="element first"><a href="/">Home</a></div>
+        <div class="element"><a href="/news">News</a></div>
         <div class="element">
-            <a href="<?=$url?>/our-office">Our Office</a>
+            <a href="/our-office">Our Office</a>
             <div class="sub-tree sub-tree-3">
-                <a href="<?=$url?>/meet-the-staff">Meet the Staff</a>
-                <a href="<?=$url?>/meet-the-dr">Meet the Doctor</a>
-                <a href="<?=$url?>/photo-gallery">Photo Gallery</a>
+                <a href="/meet-the-staff">Meet the Staff</a>
+                <a href="/meet-the-dr">Meet the Doctor</a>
+                <a href="/photo-gallery">Photo Gallery</a>
             </div>
         </div>
-        <div class="element"><a href="<?=$url?>/the-practice">The Practice</a></div>
-        <div class="element"><a href="<?=$url?>/patient-info">Patient Info</a></div>
-        <div class="element last"><a href="<?=$url?>/contact-us">Contact Us</a></div>
+        <div class="element"><a href="/the-practice">The Practice</a></div>
+        <div class="element"><a href="/patient-info">Patient Info</a></div>
+        <div class="element last"><a href="/contact-us">Contact Us</a></div>
     </div>
     <div class="page">
         <div class="content">
